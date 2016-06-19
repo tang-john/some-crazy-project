ec650338e77e50c37469c2e93d695f2407c0bfb4
johntang <john.johntang@gmail.com>
2016-06-19 11:40:30 -0700

    Added cancel button to admin user detail

--- client/src/components/admin-user-detail.js
+++ client/src/components/admin-user-detail.js
@@ -1,8 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes  } from 'react';
+import { reduxForm } from 'redux-form';
 import { Link } from 'react-router';
 
 
 class AdminUserDetail extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
 
   componentWillMount() {
     this.setState( {id: null, firstname: null, lastname: null, username: null, password: null, gender: null, dob: null});
@@ -32,36 +36,42 @@ class AdminUserDetail extends Component {
       return (
         <div>
           <h4>User Detail</h4>
-          <div className="input-group">
-            <label htmlFor="firstname">First Name</label>
-            <input type="text" className="form-control" id="firstname" value={this.state.firstname} onChange={this.handleChange}  aria-describedby="basic-addon1" />
-            <input type="hidden" className="form-control" id="id" value={this.state.id}  aria-describedby="basic-addon1" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="lasttname">Last Name</label>
-            <input type="text" className="form-control" id="lastname" value={this.state.lastname} onChange={this.handleChange} aria-describedby="basic-addon1" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="username">User Name</label>
-            <input type="text" className="form-control" id="username" value={this.state.username}  onChange={this.handleChange} aria-describedby="basic-addon1" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <input type="text" className="form-control" id="password" value={this.state.password} onChange={this.handleChange} aria-describedby="basic-addon1" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="gender">Gender</label>
-            <input type="text" className="form-control" id="gender" value={this.state.gender} onChange={this.handleChange} aria-describedby="basic-addon1" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="dob">DOB</label>
-            <input type="text" className="form-control" id="dob" value={this.state.dob} onChange={this.handleChange} aria-describedby="basic-addon1" />
-          </div>
-          <div className="adminUserSave">
-            <Link  to="admin-users-save">
-              <i className="fa fa-floppy-o  fa-2x" aria-hidden="true"></i> Save
-            </Link>
-          </div>
+          <form >
+            <div className="input-group">
+              <label htmlFor="firstname">First Name</label>
+              <input type="text" className="form-control" id="firstname" value={this.state.firstname} onChange={this.handleChange}  aria-describedby="basic-addon1" />
+              <input type="hidden" className="form-control" id="id" value={this.state.id}  aria-describedby="basic-addon1" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="lasttname">Last Name</label>
+              <input type="text" className="form-control" id="lastname" value={this.state.lastname} onChange={this.handleChange} aria-describedby="basic-addon1" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="username">User Name</label>
+              <input type="text" className="form-control" id="username" value={this.state.username}  onChange={this.handleChange} aria-describedby="basic-addon1" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input type="text" className="form-control" id="password" value={this.state.password} onChange={this.handleChange} aria-describedby="basic-addon1" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="gender">Gender</label>
+              <input type="text" className="form-control" id="gender" value={this.state.gender} onChange={this.handleChange} aria-describedby="basic-addon1" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="dob">DOB</label>
+              <input type="text" className="form-control" id="dob" value={this.state.dob} onChange={this.handleChange} aria-describedby="basic-addon1" />
+            </div>
+            <div className="adminUserSave">
+              <Link  to="admin-users-save">
+                <i className="fa fa-floppy-o  fa-2x" aria-hidden="true"></i> Save
+              </Link>
+              <Link  to="admin-users">
+                <i className="fa fa-times fa-2x" aria-hidden="true"></i> Cancel
+              </Link>
+
+            </div>
+          </form>
         </div>
       );
     }
